8677ba2437b0e573be87879230c40c9ba11e41e9
Ricardo <ricardojoseajedrez@gmail.com>
2021-01-15 14:22:53 -0400

    class-mb-added

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout>
 
     <div class="max-w-5xl mx-auto px-2 sm:px-6 md:px-8 py-8">
-
+    
         <h1 class="uppercase text-center text-3xl font-bold">Tag: {{$tag->name}}</h1>
-
+    
     </div>
    
     @foreach ($posts as $post)
