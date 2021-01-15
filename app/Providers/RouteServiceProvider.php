b1a3c1656f021d90265d6f96854018c2fa7202b9
Ricardo <ricardojoseajedrez@gmail.com>
2021-01-15 14:59:30 -0400

    jeroennoten / Laravel-AdminLTE

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
@@ -47,6 +47,12 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+        
+        Route::middleware('web', 'auth')
+        ->prefix('admin')
+        ->namespace($this->namespace)
+        ->group(base_path('routes/admin.php'));
+
     }
 
     /**
