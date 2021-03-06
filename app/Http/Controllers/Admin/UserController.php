00ab03f9b0faef4c2eb8c538b4e8b38f9fa862ec
Ricardo <ricardojoseajedrez@gmail.com>
2021-03-06 12:21:09 -0400

    update method fixed: User $user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -75,9 +75,13 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        
+     
+
+
+
     }
 
     /**
